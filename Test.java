df76bd73571ff0360c08899867e628616f682375
Prakash Subramanian <64981127+AKASANCRACY@users.noreply.github.com>
2024-11-10 21:25:06 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -3,6 +3,6 @@ public class Test
     public static void main(String []args)
     {
         for(int i=1;i<=5;i++)
-        System.out.println("My First Java Program.");
+        System.out.println("Hi hello I am Prakash.");
     }
 };
